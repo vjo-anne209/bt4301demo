706f8a23b3c206a72cd46573016c81bc30d99bb2
vjo-anne209 <77625659+vjo-anne209@users.noreply.github.com>
2024-02-29 23:41:19 +0800

    added files

---
+++ NUS/BT4301/src/mymathserver.py
@@ -0,0 +1,19 @@
+from connexion import FlaskApp
+
+
+
+app = FlaskApp(__name__)
+app.add_api('mymathserver.yml')
+
+
+
+@app.route('/')
+def index():
+
+	return 'Welcome to My Math Server'
+
+
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=5000)
